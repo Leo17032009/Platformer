3a9d1387800f838f0ee8eb403c16ee88bf1bf0e8
Leo17032009 <lgorelov09@mail.ru>
2023-05-05 19:32:21 +0300

    Fixed some scripts' names

--- Assets/Scripts/Platforms/WagonPlatform.cs
+++ Assets/Scripts/Platforms/WagonPlatform.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class IncreaseSpeedAndJump : MonoBehaviour
+public class WagonPlatform : MonoBehaviour
 {
     private PlayerController _playerController;
 
