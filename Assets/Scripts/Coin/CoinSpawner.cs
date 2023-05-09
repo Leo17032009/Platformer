da87ef7fe02602f20adf1f87351ab62a6045a028
Leo17032009 <lgorelov09@mail.ru>
2023-05-09 14:57:58 +0300

    Added FinalLevel

---
+++ Assets/Scripts/Coin/CoinSpawner.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    [SerializeField] private GameObject _coin;
+    [HideInInspector] public List<GameObject> Coins = new List<GameObject>();
+
+    private void Start()
+    {
+        for (int i = Coins.Count; i < 3; Coins.Add(_coin))
+        {
+            Vector3 coinPosition = new Vector3(Random.Range(0, gameObject.transform.localScale.x), gameObject.transform.localScale.y + 0.5f, Random.Range(0, gameObject.transform.localScale.z));
+            Instantiate(_coin, coinPosition, Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+
+        //for (int i = Coins.Count; i < 3; i++)
+        //{
+            //Vector3 coinPosition = new Vector3(Random.Range(0, gameObject.transform.localScale.x), 1.5f, Random.Range(0, gameObject.transform.localScale.z));
+            StartCoroutine(TimeForSpawn());
+            //Instantiate(_coin, coinPosition, Quaternion.identity);
+            //Coins.Add(_coin);
+        //}
+    }
+
+    private IEnumerator TimeForSpawn()
+    {
+        for (int i = Coins.Count; i < 3; Coins.Add(_coin))
+        {
+            Vector3 coinPosition = new Vector3(Random.Range(0, gameObject.transform.localScale.x), gameObject.transform.localScale.y + 0.5f, Random.Range(0, gameObject.transform.localScale.z));
+
+            yield return new WaitForSeconds(10f);
+
+            Instantiate(_coin, coinPosition, Quaternion.identity);
+        }
+    }
+}
---
+++ Assets/Scripts/Coin/CoinToSpawn.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinToSpawn : MonoBehaviour
+{
+    private CoinSpawner _coinSpawner;
+
+    private void Start()
+    {
+        _coinSpawner = FindObjectOfType<CoinSpawner>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _coinSpawner.Coins.RemoveAt(0);
+        }
+    }
+}
