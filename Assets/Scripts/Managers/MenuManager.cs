cc37c7c61afe3b197aee3cc46ea63d5f5ff94af7
Leo17032009 <lgorelov09@mail.ru>
2023-05-13 19:38:07 +0300

    Added Menu, fixed Levels

---
+++ Assets/Scripts/Managers/MenuManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] Button _startGameButton;
+    [SerializeField] Button _exitButton;
+
+    private void Awake()
+    {
+        if (SceneManager.GetActiveScene().name == "Menu")
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 0)
+        {
+            _startGameButton.onClick.AddListener(() => SceneManager.LoadScene("FirstLevel"));
+            _exitButton.onClick.AddListener(() => Application.Quit());
+        }
+    }
+
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
---
+++ Assets/Scripts/Platforms/NextLevel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NextLevel : MonoBehaviour
+{
+    private MenuManager _menuManager;
+
+    private void Start()
+    {
+        _menuManager = FindObjectOfType<MenuManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            _menuManager.NextLevel();
+        }
+    }
+}
